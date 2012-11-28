31c450280b50af9efcc3938700dd75c885c29df0
Jimmy Byrd <jimmybyrd@gmail.com>
2012-11-28 09:04:10 -0500

    Added extensions for datasource filtering

---
+++ src/kendoExtensions.js
@@ -0,0 +1,45 @@
+﻿kendo.data.DataSource.prototype.kx =function() {
+    return {
+        DataSource: this,
+        filter: function () {
+            var filters = [];
+            if (this.DataSource.filter() != undefined) {
+                filters = this.DataSource.filter().filters;
+            }
+            return filters;
+        },
+        addFilter: function (filter) {
+            var filters = this.filter();
+            filters.push(filter);
+            this.DataSource.filter(filters);
+        },
+        removeFilter: function (filter) {
+            var filters = this.filter();
+
+            filters = _(filters).reject(function (f) {
+                return f.Id == filter.Id;
+            });
+
+            this.DataSource.filter(filters);
+        },
+        containsFilter: function (filterId) {
+            var filters = this.filter();
+            return _(_(filters).pluck("Id")).contains(filterId);
+        }
+    }
+
+};
+
+
+kendo.data.ObservableArray.prototype.kx = function () {
+    return {
+        ObservableArray: this,
+        sort: function (sortFunction) {
+            Array.prototype.sort.call(this.ObservableArray, sortFunction);
+        },
+        reverse: function () {
+            Array.prototype.reverse.call(this.ObservableArray);
+        }
+
+    };
+};
